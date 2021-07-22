7fe13b4a40635a1fcfb0f1dbb18de99e1db49e3a
Alan Souza Fagundes <alan.souza@solutis.com.br>
2021-07-22 20:48:08 -0300

    commit: respeitando variavel de ambiente port

--- main.go
+++ main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
-
+	"os"
 //	"eddy.com/todo/data"
 	"eddy.com/todo/route"
 )
 
 func main() {
-
+	var PORT = ":9000"
 	//data.InitDatabase()
 
-	log.Print("Server on")
-	http.ListenAndServe(":9000", route.RegisterRoute())
+	if envPort := os.Getenv("PORT"); envPort != "" {
+		PORT = ":" + envPort
+	}
+
+	log.Print("Server on port: ", PORT)
+	http.ListenAndServe(PORT, route.RegisterRoute())
 }
