cd924ca61f11a9a5945e22d8c60cc89f862385c7
Alan Souza Fagundes <alan.souza@solutis.com.br>
2021-07-23 19:38:59 -0300

    erro proposital

--- handler/todohandler.go
+++ handler/todohandler.go
@@ -12,7 +12,7 @@ import (
 )
 
 func Health(responseWriter http.ResponseWriter, request *http.Request) {
-	fmt.Fprint(responseWriter, "Server mais pra cá do que pra lá")
+	fmt.Fprint(responseWriter, "Server mais pra cá do que pra lá")z
 }
 
 func Insert(responseWriter http.ResponseWriter, request *http.Request) {
