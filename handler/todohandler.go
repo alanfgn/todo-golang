db5f0e87243dcfcdc31d643711db4589c30dff84
Alan Souza Fagundes <alan.souza@solutis.com.br>
2021-07-23 19:40:45 -0300

    correçao

--- handler/todohandler.go
+++ handler/todohandler.go
@@ -12,7 +12,7 @@ import (
 )
 
 func Health(responseWriter http.ResponseWriter, request *http.Request) {
-	fmt.Fprint(responseWriter, "Server mais pra cá do que pra lá")z
+	fmt.Fprint(responseWriter, "Server mais pra cá do que pra lá")
 }
 
 func Insert(responseWriter http.ResponseWriter, request *http.Request) {
